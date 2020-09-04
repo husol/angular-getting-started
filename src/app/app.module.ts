46c6f34cd6d5aad96af8953f9fe154ba23c94af8
Huynh Tan Khoa <khoa@husol.org>
2020-09-04 10:41:12 +0700

    Improve Angular Material & TailwindCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,35 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { StoriesComponent } from './components/stories/stories.component';
-import { StoryDetailComponent } from './components/story-detail/story-detail.component';
-import { MessagesComponent } from './components/messages/messages.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {StoriesComponent} from './components/stories/stories.component';
+import {StoryDetailComponent} from './components/story-detail/story-detail.component';
+import {MessagesComponent} from './components/messages/messages.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        DashboardComponent,
-        StoriesComponent,
-        StoryDetailComponent,
-        MessagesComponent,
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpClientModule,
-        AppRoutingModule,
-        BrowserAnimationsModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    StoriesComponent,
+    StoryDetailComponent,
+    MessagesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
